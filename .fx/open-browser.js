c8e73d36e5f9c2008b477da3613a97ca44696722
Aocheng Wang <aochengwang@microsoft.com>
2022-10-27 15:05:33 +0800

    chore: fix open package installation

--- .fx/open-browser.js
+++ .fx/open-browser.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
-const open = require("open");
+const path = require("path");
+const child_process = require("child_process");
 
-const localStateString = fs.readFileSync(".fx/states/state.local.json", "utf-8");
+const localStateString = fs.readFileSync(path.join(__dirname, "states", "/state.local.json"), "utf-8");
 const localState = JSON.parse(localStateString);
 const teamsAppId = localState["fx-resource-appstudio"].teamsAppId;
-open(`https://teams.microsoft.com/_#/l/app/${teamsAppId}?installAppPackage=true&webjoin=true`)
+const url = `https://teams.microsoft.com/l/app/${teamsAppId}?installAppPackage=true&webjoin=true`;
+child_process.execFile("npx", ["open-cli", url], { shell: false });
